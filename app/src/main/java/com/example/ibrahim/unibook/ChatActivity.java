136cf5e0df2b5744fadd99408f757525e6315c2b
ibolight91 <ibolight91@gmail.com>
2017-02-25 13:02:53 +0100

    this is a test

--- app/src/main/java/com/example/ibrahim/unibook/ChatActivity.java
+++ app/src/main/java/com/example/ibrahim/unibook/ChatActivity.java
@@ -2,6 +2,7 @@ package com.example.ibrahim.unibook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        Toast.makeText(this, "this is a test for github", Toast.LENGTH_SHORT).show();
     }
 }
