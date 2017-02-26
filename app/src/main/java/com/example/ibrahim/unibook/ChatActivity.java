2c1d205362175c6f15eace4740d3df512c0bcf99
Billy <bpapageo@gmail.com>
2017-02-26 21:18:32 +0100

    Chat has been added

--- app/src/main/java/com/example/ibrahim/unibook/ChatActivity.java
+++ app/src/main/java/com/example/ibrahim/unibook/ChatActivity.java
@@ -1,128 +1,111 @@
 package com.example.ibrahim.unibook;
 
-    import android.content.DialogInterface;
-    import android.content.Intent;
-    import android.os.Bundle;
-    import android.support.v7.app.AlertDialog;
-    import android.support.v7.app.AppCompatActivity;
-    import android.view.View;
-    import android.widget.AdapterView;
-    import android.widget.ArrayAdapter;
-    import android.widget.Button;
-    import android.widget.EditText;
-    import android.widget.ListView;
-    import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
-    import com.google.firebase.database.DataSnapshot;
-    import com.google.firebase.database.DatabaseError;
-    import com.google.firebase.database.DatabaseReference;
-    import com.google.firebase.database.FirebaseDatabase;
-    import com.google.firebase.database.ValueEventListener;
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
-    import java.util.ArrayList;
-    import java.util.HashMap;
-    import java.util.HashSet;
-    import java.util.Iterator;
-    import java.util.Map;
-    import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
-    public class ChatActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity {
+    LinearLayout layout;
+    ImageView sendButton;
+    EditText messageArea;
+    ScrollView scrollView;
+    Firebase reference1, reference2;
 
-        private Button  add_room;
-        private EditText room_name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
 
-        private ListView listView;
-        private ArrayAdapter<String> arrayAdapter;
-        private ArrayList<String> list_of_rooms = new ArrayList<>();
-        private String name;
-        private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+        layout = (LinearLayout)findViewById(R.id.layout1);
+        sendButton = (ImageView)findViewById(R.id.sendButton);
+        messageArea = (EditText)findViewById(R.id.messageArea);
+        scrollView = (ScrollView)findViewById(R.id.scrollView);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_chat);
+        Firebase.setAndroidContext(this);
+        reference1 = new Firebase("https://android-chat-app-e711d.firebaseio.com/messages/" + UserDetails.username + "_" + UserDetails.chatWith);
+        reference2 = new Firebase("https://android-chat-app-e711d.firebaseio.com/messages/" + UserDetails.chatWith + "_" + UserDetails.username);
 
-            add_room = (Button) findViewById(R.id.btn_add_room);
-            room_name = (EditText) findViewById(R.id.room_name_edittext);
-            listView = (ListView) findViewById(R.id.listView);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = messageArea.getText().toString();
 
-            arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list_of_rooms);
-
-            listView.setAdapter(arrayAdapter);
-
-            request_user_name();
-
-            add_room.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    Map<String,Object> map = new HashMap<String, Object>();
-                    map.put(room_name.getText().toString(),"");
-                    root.updateChildren(map);
-
+                if(!messageText.equals("")){
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("message", messageText);
+                    map.put("user", UserDetails.username);
+                    reference1.push().setValue(map);
+                    reference2.push().setValue(map);
                 }
-            });
+            }
+        });
 
-            root.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
+        reference1.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map map = dataSnapshot.getValue(Map.class);
+                String message = map.get("message").toString();
+                String userName = map.get("user").toString();
 
-                    Set<String> set = new HashSet<String>();
-                    Iterator i = dataSnapshot.getChildren().iterator();
-
-                    while (i.hasNext()){
-                        set.add(((DataSnapshot)i.next()).getKey());
-                    }
-
-                    list_of_rooms.clear();
-                    list_of_rooms.addAll(set);
-
-                    arrayAdapter.notifyDataSetChanged();
+                if(userName.equals(UserDetails.username)){
+                    addMessageBox("You:-\n" + message, 1);
                 }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
+                else{
+                    addMessageBox(UserDetails.chatWith + ":-\n" + message, 2);
                 }
-            });
+            }
 
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-                    Intent intent = new Intent(getApplicationContext(),Chat_Room.class);
-                    intent.putExtra("room_name",((TextView)view).getText().toString() );
-                    intent.putExtra("user_name",name);
-                    startActivity(intent);
-                }
-            });
+            }
 
-        }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
 
-        private void request_user_name() {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Enter name:");
+            }
 
-            final EditText input_field = new EditText(this);
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
 
-            builder.setView(input_field);
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    name = input_field.getText().toString();
-                }
-            });
+            }
 
-            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    dialogInterface.cancel();
-                    request_user_name();
-                }
-            });
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
 
-            builder.show();
-        }
-
-
+            }
+        });
     }
+
+    public void addMessageBox(String message, int type){
+        TextView textView = new TextView(ChatActivity.this);
+        textView.setText(message);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, 0, 0, 10);
+        textView.setLayoutParams(lp);
+
+        if(type == 1) {
+            textView.setBackgroundResource(R.drawable.rounded_corner1);
+        }
+        else{
+            textView.setBackgroundResource(R.drawable.rounded_corner2);
+        }
+
+        layout.addView(textView);
+        scrollView.fullScroll(View.FOCUS_DOWN);
+    }
+}
---
+++ app/src/main/java/com/example/ibrahim/unibook/UserDetails.java
@@ -0,0 +1,11 @@
+package com.example.ibrahim.unibook;
+
+/**
+ * Created by Billy on 2/26/2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
